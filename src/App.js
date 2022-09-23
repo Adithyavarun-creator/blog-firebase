c7721860d74f4402e7368591a3431f453f823e73
Adithyavarun-creator <74570395+Adithyavarun-creator@users.noreply.github.com>
2022-09-23 17:51:24 +0530

    blog updates

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./App.css";
+import Articles from "./components/Articles";
+import AddArticle from "./components/AddArticle";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Article from "./components/Article";
+
+
+function App() {
+  return (
+    <div className="container">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/register" element={<Register />} />
+
+          <Route path="/signin" element={<Login />} />
+
+          <Route path="/article/:id" element={<Article />} />
+
+          <Route
+            path="/"
+            element={
+              <div className="row">
+                <div className="col-md-8">
+                  <Articles />
+                </div>
+
+                <div className="col-md-4">
+                  <AddArticle />
+                </div>
+              </div>
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
